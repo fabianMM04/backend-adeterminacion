ce7fcb6b6c3912bdfb604d0ed8bdb1103fe48c77
Fabian Martinez <fabianmar@gohubtek.com>
2023-11-05 18:00:39 -0500

    upload file historico

--- models/historico08.js
+++ models/historico08.js
@@ -1,17 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const historicoochoSchema = new mongoose.Schema({
-    REF_CATASTRAL_1: String,
-    REF_CATASTRAL_2: String,
-    No_RESOLUCION: String,
-    No_EXPEDIENTE: String,
-    FECHA: String,
-    VIG_DETERMINADAS: String,
-    HOJA: String,
-    NOTIFICADO_DEVUELTO: String,
-    TOTAL_DETERMINADO: String
-})
+  REF_CATASTRAL_1: String,
+  REF_CATASTRAL_2: String,
+  No_RESOLUCION: String,
+  No_EXPEDIENTE: String,
+  FECHA: String,
+  VIG_DETERMINADAS: String,
+  HOJA: String,
+  NOTIFICADO_DEVUELTO: String,
+  TOTAL_DETERMINADO: String,
+  status: String,
+});
 
-const HistoricoOcho = mongoose.model('historicoocho', historicoochoSchema);
+const HistoricoOcho = mongoose.model("historicoocho", historicoochoSchema);
 
 module.exports = HistoricoOcho;
--- routes/historicoseisonce.js
+++ routes/historicoseisonce.js
@@ -1,116 +1,130 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const  Historico  = require('../models/historico0611');
-const moment = require('moment');
-var multer = require('multer');
+const Historico = require("../models/historico0611");
+const moment = require("moment");
+var multer = require("multer");
+const csv = require("csvtojson");
 
-var upload = multer({ dest: 'uploads/' });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + ".csv");
+  },
+});
 
-router.post('/upload_files', upload.single('file'), (req, res, next) => {
-    csv()
+const upload = multer({ storage: storage });
+
+router.post("/upload_files", upload.single("file"), (req, res, next) => {
+  csv({ delimiter: "auto" })
     .fromFile(req.file.path)
-    .then((jsonObj)=>{
-        var historico = [];
-        for(var i = 0;i<jsonObj.length;i++){
-            var obj={};
-            obj.REFERENCIA=jsonObj[i]['REFERENCIA'];
-            obj.REF_CATASTRAL=jsonObj[i]['REF_CATASTRAL'];
-            obj.No_RESOLUCION=jsonObj[i]['No_RESOLUCION'];
-            obj.No_EXPEDIENTE=jsonObj[i]['No_EXPEDIENTE'];
-            obj.FECHA=jsonObj[i]['FECHA'];
-            obj.VIG_DETERMINADAS=jsonObj[i]['VIG_DETERMINADAS'];
-            obj.GRUPO=jsonObj[i]['GRUPO'];
-            obj.NOTIFICADO_DEVUELTO=jsonObj[i]['NOTIFICADO_DEVUELTO'];
-            obj.BUSQUEDA=jsonObj[i]['BUSQUEDA'];
-            obj.TOTAL_DETERMINADO=jsonObj[i]['TOTAL_DETERMINADO'];
-            obj.NO_IMAGE_SCANED=jsonObj[i]['NO_IMAGE_SCANED'];
+    .then((jsonObj) => {
+      var historico = [];
+      for (var i = 0; i < jsonObj.length; i++) {
+        var obj = {};
+        obj.REFERENCIA = jsonObj[i]["REFERENCIA"];
+        obj.REF_CATASTRAL = jsonObj[i]["REF_CATASTRAL"];
+        obj.No_RESOLUCION = jsonObj[i]["No_RESOLUCION"];
+        obj.No_EXPEDIENTE = jsonObj[i]["No_EXPEDIENTE"];
+        obj.FECHA = jsonObj[i]["FECHA"];
+        obj.VIG_DETERMINADAS = jsonObj[i]["VIG_DETERMINADAS"];
+        obj.GRUPO = jsonObj[i]["GRUPO"];
+        obj.NOTIFICADO_DEVUELTO = jsonObj[i]["NOTIFICADO_DEVUELTO"];
+        obj.BUSQUEDA = jsonObj[i]["BUSQUEDA"];
+        obj.TOTAL_DETERMINADO = jsonObj[i]["TOTAL_DETERMINADO"];
+        obj.NO_IMAGE_SCANED = jsonObj[i]["NO_IMAGE_SCANED"];
+        obj.status = "ACTIVO";
 
-            historico.push(obj);
-        }
-        Historico.insertMany(historico).then(function(){
-            res.status(200).send({
-                message: "Successfully Uploaded!"
-            });
-        }).catch(function(error){
-            res.status(500).send({
-                message: "failure",
-                error
-            });
-        });
-    }).catch((error) => {
-        res.status(500).send({
+        historico.push(obj);
+      }
+      Historico.insertMany(historico)
+        .then(function () {
+          res.status(200).send({
+            message: "Successfully Uploaded!",
+          });
+        })
+        .catch(function (error) {
+          res.status(500).send({
             message: "failure",
-            error
+            error,
+          });
         });
     })
-});
-
-router.get('/',  (req, res) =>{
-    Historico.find({}).exec(async(err, asignacionesAD) =>{
-        if(err){
-            res.status(500).send({
-                message: "Request error"
-            });
-        }else{
-            if(!asignacionesAD){
-                res.status(404).send({
-                    message: "No se han encontrado datos historicos."
-                });
-            }else{
-                console.log("show historico")
-                res.status(200).send({
-                    historico: asignacionesAD,
-                });
-            }
-        }
+    .catch((error) => {
+      res.status(500).send({
+        message: "failure",
+        error,
+      });
     });
-    
 });
 
-
-router.get('/:id', async (req, res) =>{
-    let id = req.params.id;
-    let historico = await Historico.findById(id);
-    if(!historico) return res.status(404).send('El historico no fue encontrado.');
-     
-    res.send({historico});
+router.get("/", (req, res) => {
+  Historico.find({}).exec(async (err, asignacionesAD) => {
+    if (err) {
+      res.status(500).send({
+        message: "Request error",
+      });
+    } else {
+      if (!asignacionesAD) {
+        res.status(404).send({
+          message: "No se han encontrado datos historicos.",
+        });
+      } else {
+        console.log("show historico");
+        res.status(200).send({
+          historico: asignacionesAD,
+        });
+      }
+    }
+  });
 });
 
-router.post('/', async(req, res) => {
+router.get("/:id", async (req, res) => {
+  let id = req.params.id;
+  let historico = await Historico.findById(id);
+  if (!historico)
+    return res.status(404).send("El historico no fue encontrado.");
 
-    let historico = new Historico({
-        REFERENCIA: req.body.REFERENCIA,
-        REF_CATASTRAL: req.body.REF_CATASTRAL,
-        No_RESOLUCION: req.body.No_RESOLUCION,
-        No_EXPEDIENTE: req.body.No_EXPEDIENTE,
-        FECHA: req.body.FECHA,
-        VIG_DETERMINADAS: req.body.VIG_DETERMINADAS,
-        GRUPO: req.body.GRUPO,
-        NOTIFICADO_DEVUELTO: req.body.NOTIFICADO_DEVUELTO,
-        BUSQUEDA: req.body.BUSQUEDA,
-        TOTAL_DETERMINADO: req.body.TOTAL_DETERMINADO,
-        NO_IMAGE_SCANED: req.body.NO_IMAGE_SCANED,
-    });
-    
-    historico = await historico.save();
-    if(!historico) return res.status(404).send("No se guardó el historico!");
-    res.send({historico});
+  res.send({ historico });
 });
 
-router.put('/:id', async (req, res) =>{
+router.post("/", async (req, res) => {
+  let historico = new Historico({
+    REFERENCIA: req.body.REFERENCIA,
+    REF_CATASTRAL: req.body.REF_CATASTRAL,
+    No_RESOLUCION: req.body.No_RESOLUCION,
+    No_EXPEDIENTE: req.body.No_EXPEDIENTE,
+    FECHA: req.body.FECHA,
+    VIG_DETERMINADAS: req.body.VIG_DETERMINADAS,
+    GRUPO: req.body.GRUPO,
+    NOTIFICADO_DEVUELTO: req.body.NOTIFICADO_DEVUELTO,
+    BUSQUEDA: req.body.BUSQUEDA,
+    TOTAL_DETERMINADO: req.body.TOTAL_DETERMINADO,
+    NO_IMAGE_SCANED: req.body.NO_IMAGE_SCANED,
+    status: "ACTIVO",
+  });
 
-    let id = req.params.id;
-    let historico = await Historico.findByIdAndUpdate(id, req.body, {new: true});
-    if(!historico) return res.status(404).send('El historico no se encontró.');    
-    res.status(200).send({historico});
+  historico = await historico.save();
+  if (!historico) return res.status(404).send("No se guardó el historico!");
+  res.send({ historico });
 });
 
-router.delete('/:id', async (req, res) =>{
-    let id = req.params.id;
-    let historico = await Historico.findByIdAndRemove(id);
-    if(!historico) return res.status(404).send('El historico no se encontró.');
-    res.send({historico});
+router.put("/:id", async (req, res) => {
+  let id = req.params.id;
+  let historico = await Historico.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  if (!historico) return res.status(404).send("El historico no se encontró.");
+  res.status(200).send({ historico });
 });
 
+router.delete("/:id", async (req, res) => {
+  let id = req.params.id;
+  let historico = await Historico.findByIdAndRemove(id);
+  if (!historico) return res.status(404).send("El historico no se encontró.");
+  res.send({ historico });
+});
 
 module.exports = router;
